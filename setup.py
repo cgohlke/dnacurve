79c50b7bb5e8a57a86c90baa19b13c507ce3e3b1
Christoph Gohlke <cgohlke@cgohlke.com>
2022-10-04 10:11:15 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,34 +1,43 @@
 # dnacurve/setup.py
 
-"""Dnacurve package setuptools script."""
+"""Dnacurve package Setuptools script."""
 
 import sys
 import re
 
 from setuptools import setup
 
+
+def search(pattern, code, flags=0):
+    # return first match for pattern in code
+    match = re.search(pattern, code, flags)
+    if match is None:
+        raise ValueError(f'{pattern!r} not found')
+    return match.groups()[0]
+
+
 with open('dnacurve/dnacurve.py') as fh:
     code = fh.read()
 
-version = re.search(r"__version__ = '(.*?)'", code).groups()[0]
+version = search(r"__version__ = '(.*?)'", code)
 
-description = re.search(r'"""(.*)\.(?:\r\n|\r|\n)', code).groups()[0]
+description = search(r'"""(.*)\.(?:\r\n|\r|\n)', code)
 
-readme = re.search(
-    r'(?:\r\n|\r|\n){2}"""(.*)"""(?:\r\n|\r|\n){2}__version__',
+readme = search(
+    r'(?:\r\n|\r|\n){2}"""(.*)"""(?:\r\n|\r|\n){2}from __future__',
     code,
     re.MULTILINE | re.DOTALL,
-).groups()[0]
+)
 
 readme = '\n'.join(
     [description, '=' * len(description)] + readme.splitlines()[1:]
 )
 
-license = re.search(
+license = search(
     r'(# Copyright.*?(?:\r\n|\r|\n))(?:\r\n|\r|\n)+""',
     code,
     re.MULTILINE | re.DOTALL,
-).groups()[0]
+)
 
 license = license.replace('# ', '').replace('#', '')
 
@@ -42,26 +51,27 @@ if 'sdist' in sys.argv:
 setup(
     name='dnacurve',
     version=version,
+    license='BSD',
     description=description,
     long_description=readme,
     author='Christoph Gohlke',
-    author_email='cgohlke@uci.edu',
-    url='https://www.lfd.uci.edu/~gohlke/dnacurve',
+    author_email='cgohlke@cgohlke.com',
+    url='https://www.cgohlke.com',
     project_urls={
         'Bug Tracker': 'https://github.com/cgohlke/dnacurve/issues',
         'Source Code': 'https://github.com/cgohlke/dnacurve',
         # 'Documentation': 'https://',
     },
     packages=['dnacurve'],
-    python_requires='>=3.7',
-    install_requires=['numpy>=1.15.1', 'matplotlib'],
+    python_requires='>=3.8',
+    install_requires=['numpy>=1.19.2', 'matplotlib'],
+    extras_require={'all': ['Flask']},
     entry_points={
         'console_scripts': [
             'dnacurve = dnacurve.dnacurve:main',
-            'dnacurve_web = dnacurve.dnacurve_web:main',
+            'dnacurve_web = dnacurve.web:main',
         ]
     },
-    license='BSD',
     platforms=['any'],
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -70,9 +80,9 @@ setup(
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
 )
